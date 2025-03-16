485e12f77b8b20e0c3b7d9e3ac00d7d8434e6f9b
unknown <remeshkrishnaur@gmail.com>
2025-03-17 00:59:59 +0530

    modified authentication

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_headers= ['*']
     
     )
-tasks = []
+tasks = {}
 users = []
 
 class User(BaseModel):
@@ -60,6 +60,7 @@ class Status(str,Enum):
     completed = "Completed"
 
 class Task(BaseModel):
+    id: int | None = 0
     name: str
     description: str | None = None
     due_date: date
@@ -73,7 +74,7 @@ def authenticate(username,password):
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User doesn't exist")
     if not verify_password(user[0].password,password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid Credentials", headers={"WWW-Authenticate":"Bearer"})
 
 
 def create_hashed_password(password):
@@ -88,6 +89,19 @@ def create_access_token(data: dict):
     updated_data.update({"exp":expire_time})
     encoded_data = jwt.encode(payload=updated_data,key=SECRET_KEY,algorithm=ALGORITHM)
     return encoded_data
+
+def decode_access_token(token: Annotated[str,Depends(oauth2_scheme)]):
+    token_obj = jwt.decode(token,key=SECRET_KEY,algorithms=[ALGORITHM])
+    print(token_obj)
+    username=token_obj["sub"]
+    return username
+
+def get_current_user(username: Annotated[str,Depends(decode_access_token)]):
+    user_found = [u for u in users if u.email==username]
+    if not user_found:
+        raise HTTPException(status_code=404,detail="Invalid user")
+    return user_found
+
 
 @app.get('/',include_in_schema=False)#This will be excluded from documentation
 def home():
@@ -116,10 +130,6 @@ def signup(user: SignupInput):
     new_user = User(id=len(users)+1,email = user.email, password=create_hashed_password(user.password),name= user.name)
     users.append(new_user)
     return new_user
-
-@app.get('/items')
-def get_items(token: Annotated[str,Depends(oauth2_scheme)]):
-    pass
 
 @app.post('/token',
           responses={
@@ -152,54 +162,19 @@ def get_access_token(formData: Annotated[OAuth2PasswordRequestForm,Depends()]):
     access_token = create_access_token({"sub":formData.username})
     return {"access_token":access_token, "token_type":"bearer"}
 
-# @app.post("/signin",
-#           response_model=Union[UserOutput , dict],
-#           tags=["users"],
-#           status_code=200,
-#           responses={
-#               401: {
-#                   "description": "Unauthorized",
-#                   "content":{
-#                       "application/json":{
-#                           "example":{"detail":"Invalid credentials"}
-#                       }
-#                   }
-#               },
-#               404: {
-#                   "description":"User not found",
-#                   "content":{
-#                       "application/json":{
-#                           "example": {"detail":"User not exist"}
-#                       }
-#                   }
-#               }
-#           }) #we can use either Union or | We selected Union because it is backward compatible..and | is introduced in python 3.10 
-# def signin(user: LoginInput):
-#     user_found = [i for i in users if i.email==user.email]
-#     print(user_found)
-#     if user_found:
-#         if user.password == user_found[0].password:
-#             return user_found[0]
-#         else:
-#             raise HTTPException(
-#                 status_code=status.HTTP_401_UNAUTHORIZED,
-#                 detail="Invalid credentials"
-#                 )
-#     else:
-#         raise HTTPException(
-#                 status_code=status.HTTP_404_NOT_FOUND,
-#                 detail="User not exist"
-#                 )
-
 @app.get('/users' ,response_model=list[UserOutput],status_code=200, tags=["users"])
 def get_all_users():
     return users
 
 @app.get('/tasks' ,response_model=list[Task],status_code=200,tags=["tasks"])
-def get_tasks():
-    return tasks
+def get_tasks(user: Annotated[User,Depends(get_current_user)] ):
+    return tasks[user.email]
 
 @app.post('/tasks',status_code=201,tags=["tasks"])
-def add_task(task: Task):
-    tasks.append(task)
+def add_task(task: Task, user: Annotated[list[User],Depends(get_current_user)]):
+    task.id = len(tasks)+1
+    if user[0].email in tasks:
+        tasks[user[0].email].append(task)
+    else:
+        tasks[user[0].email] = [task]
     return task
